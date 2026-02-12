3d06dc9b6e33463de6ba1239a6fa983e1bf1f50a
Ofentse <ofentsemakhutja@icloud.com>
2026-02-12 22:35:08 +0200

    add getNotaion function

--- app.js
+++ app.js
@@ -29,6 +29,17 @@ let boardState = [
   ["R", "N", "B", "Q", "K", "B", "N", "R"],
 ];
 
+let squareCoordinates = [
+  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
+];
+
 const pieceIcons = {
   p: "♟",
   r: "♜",
@@ -225,6 +236,7 @@ function onSquareClick(row, col) {
     // Define turn finalization (called immediately or after promotion)
     const finalizeTurn = () => {
       updateCastlingRights(pieceChar, startRow, startCol);
+      console.log(getNotation(pieceChar, row, col, squareCoordinates));
 
       // Set En Passant Target
       if (pieceChar.toLowerCase() === "p" && Math.abs(row - startRow) === 2) {
@@ -443,6 +455,18 @@ function showPromotionUI(row, col, color, onSelect) {
     `[data-row="${row}"][data-col="${col}"]`,
   );
   square.appendChild(promotionDiv);
+}
+
+function getNotation(pieceChar, r, c, squareCoordinates) {
+  color = pieceChar === pieceChar.toUpperCase() ? "white" : "black";
+  const kingLoc = findKing(boardState, color);
+  const piece = pieceChar.toUpperCase() === "P" ? "" : pieceChar.toUpperCase();
+  const square = squareCoordinates[r][c];
+  let notation = `${piece}${square}`;
+  if (isSquareUnderAttack(kingLoc.row, kingLoc.col, boardState, color)) {
+    return notation + "+";
+  }
+  return notation;
 }
 
 flipBtn.addEventListener("click", () => {
